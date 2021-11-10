55ef1c4b01cd95202628a40cce60b20ba07a4128
mia-thomas <miathomas1102@gmail.com>
2021-11-10 17:33:53 +0000

    more mini challenges & updates

---
+++ conditionals.py
@@ -0,0 +1,9 @@
+temperature = 31
+
+if temperature > 30:
+    print("today is hot!")
+elif temperature > 20:
+    print("the weather today is ok, no need for a coat!")
+else:
+    print("grab a coat!!")
+
---
+++ whileLoop.py
@@ -0,0 +1,11 @@
+x = 5
+y = 7
+
+print(x)
+print(y)
+
+while x == y:
+    print("they match!")
+    break
+else:
+    print("They do not match!")
