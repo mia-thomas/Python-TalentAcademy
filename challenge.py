0e2400233716a9ebc338fcee32cbffa8dbd51ad4
mia-thomas <miathomas1102@gmail.com>
2021-11-11 15:11:51 +0000

    day2 python learning

--- challenge.py
+++ challenge.py
@@ -5,16 +5,15 @@ appleWeightKG = (1 / 5)
 appleWeightLBS = (2.2 / 5)
 
 appleRequest = int(input("Hello, how many apples would you like today?"))
-weightRequest = input("Great! would you like them weighed in LBS or KG?")
+weightRequest = str.lower(input("Great! would you like them weighed in LBS or KG?"))
 
 
-if weightRequest == "lbs":
+if weightRequest == "lbs" or weightRequest == "LBS":
     lbsApple = appleWeightLBS * appleRequest
     print("Your Apples Weigh:", lbsApple, "lbs") 
-elif weightRequest == "kg":
+elif weightRequest == "kg" or weightRequest == "KG": 
     kgApple = appleWeightKG * appleRequest
     print("Your Apples Weigh:", kgApple, "kg")
 else:
     print("Please try again. Invalid input")
 
-    
